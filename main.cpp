5d03a829aafeb6ca8fa1cc4d7cc27b38e9961d00
AndreiBGheorghe <andrei-bogdan.gheorghe@s.unibuc.ro>
2023-10-31 13:32:15 +0200

    Tema 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,115 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+class Caracter {
+private:
+    string nume;
+    int movement;
+
+public:
+    ~Caracter(){cout<<"A fost apelat \n";}
+    explicit Caracter(const string& name = "Fork", const int& move = 1) {
+        this->nume = name;
+        this->movement = move;
+    }
+
+    [[nodiscard]] string getnume() const {
+        return this->nume;
+    }
+
+    void setnume(const string &name) {
+        this->nume = name;
+    }
+
+    [[nodiscard]] int getmovement() const {
+        return this->movement;
+    }
+
+    void setmovement(const int &move) {
+        this->movement = move;
+    }
+
+    friend ostream &operator<<(ostream &out, const Caracter &c);
+    friend istream &operator>>(istream &in, Caracter &c);
+};
+
+istream  &operator>>(istream &in,  Caracter &c) {
+    cout << "Caracterul este: " ;
+    in >> c.nume;
+    cout << "Caracterul are movement-ul: ";
+    in >> c.movement;
+    return in;
+}
+ostream &operator<<(ostream &out, const Caracter &c) {
+    out << "Caracterul este: " << c.nume << endl;
+    out << "Caracterul are movement-ul: " << c.movement << endl;
+    return out;
+}
+
+class Obstacol {
+private:
+    int lungime;
+    int latime;
+
+public:
+    ~Obstacol() { cout << "A fost apelat" << endl; }
+    explicit Obstacol(const int& lung = 1 , const int& lat = 1) {
+        this->lungime = lung;
+        this->latime = lat;
+    }
+
+    [[nodiscard]] int getlungime() const {
+        return this->lungime;
+    }
+
+    void setlungime(int lung) {
+        this->lungime = lung;
+    }
+
+    [[nodiscard]] int getlatime() const {
+        return this->latime;
+    }
+
+    void setlatime(int lat) {
+        this->latime = lat;
+    }
+
+    friend ostream &operator<<(ostream &out, const Obstacol &obs);
+    friend istream &operator>>(istream &in, Obstacol &obs);
+};
+
+ostream &operator<<(ostream &out, const Obstacol &obs) {
+    out << "Lungimea obstacolului este: " << obs.latime << endl;
+    out << "Latimea obstacolului este: " << obs.lungime << endl;
+    return out;
+}
+istream  &operator>>(istream &in, Obstacol &obs) {
+    cout << "Lungimea obstacolului este: " ;
+    in >> obs.lungime;
+    cout << "Latimea obstacolului este: ";
+    in >> obs.latime;
+    return in;
+}
 
 int main() {
-    std::cout << "Hello, world!\n";
+
+    Caracter c1("Fork", 1);
+    cout << c1;
+    Caracter c2;
+    c2.setnume("Knife");
+    c2.setmovement(2);
+    cout << "Caracterul este : " << c2.getnume() << " \n";
+    cout << "Caracterul are movement-ul: " << c2.getmovement() << " \n";
+
+    Obstacol obs1(1,1);
+    cout << obs1;
+    Obstacol obs2;
+    obs2.setlungime(3);
+    obs2.setlatime(4);
+    cout << "Lungimea obstacolului este : "<< obs2.getlungime()<< "\n";
+    cout << "Latimea obstacolului este: "<< obs2.getlatime()<<"\n";
+
     return 0;
 }
